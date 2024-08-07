1cc65c1da9ce31450bd319a185b0bc64d29743b8
RealArtsn <105236007+RealArtsn@users.noreply.github.com>
2024-08-07 15:16:05 -0400

    Display only physical disk info

--- specs.sh
+++ specs.sh
@@ -1,13 +1,12 @@
 RAM=$(expr $(sysctl -n hw.memsize) / $((1024**3)))
 CPU=$(sysctl -a | grep brand_string | awk -F ':' '{print $2}')
-HDD1=$(diskutil info disk0 | grep 'Disk Size' | awk -F ' ' '{print $3$4}')
-HDD2=$(diskutil info disk1 | grep 'Disk Size' | awk -F ' ' '{print $3$4}')
+DISK=$(diskutil list | grep physical | awk -F ' ' '{print $1}')
+CAPACITY=$(diskutil info $DISK | grep 'Disk Size' | awk -F ' ' '{print $3$4}')
 MAX=$(ioreg -l -w0 | grep '\"MaxCapacity\" =' | awk -F ' ' '{print $5}') 
 DESIGN=$(ioreg -l -w0 | grep '\"DesignCapacity\" =' | awk -F ' ' '{print $5}') 
 BATT=$((100*$MAX/$DESIGN))
 
 echo 'CPU: '$CPU
 echo 'RAM: '$RAM'GB'
-echo 'Storage 1: '$HDD1
-echo 'Storage 2: '$HDD2
+echo 'Physical Storage: '$CAPACITY
 echo 'Battery Health: '$BATT'%'
