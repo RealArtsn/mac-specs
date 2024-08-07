51fe1c8cb87b08ce656ba3b340e18cd47448564d
RealArtsn <105236007+RealArtsn@users.noreply.github.com>
2024-08-07 10:51:32 -0400

    Print specs

---
+++ specs.sh
@@ -0,0 +1,13 @@
+RAM=$(expr sysctl -n hw.memsize) / $((1024**3))
+CPU=$(sysctl -a | grep brand_string | awk -F ':' '{print $2}')
+HDD1=$(diskutil info disk0 | grep 'Disk Size' | awk -F ' ' '{print $2$3}')
+HDD2=$(diskutil info disk1 | grep 'Disk Size' | awk -F ' ' '{print $2$3}')
+MAX=$(ioreg -l -w0 | grep '\"MaxCapacity\" =' | awk -F ' ' '{print $5}') 
+DESIGN=$(ioreg -l -w0 | grep '\"DesignCapacity\" =' | awk -F ' ' '{print $5}') 
+BATT=$((100*$MAX/$DESIGN))
+
+echo 'CPU: '$CPU
+echo 'RAM: '$RAM'GB'
+echo 'Storage 1: '$HDD1
+echo 'Storage 2: '$HDD2
+echo 'Battery Health: '$BATT'%"
