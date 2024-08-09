8f1699be7f60660638a908b171d02e26690be2b9
RealArtsn <105236007+RealArtsn@users.noreply.github.com>
2024-08-09 09:49:08 -0400

    Use more encompassing disk grep keyword

--- specs.sh
+++ specs.sh
@@ -1,11 +1,10 @@
 RAM=$(expr $(sysctl -n hw.memsize) / $((1024**3)))
 CPU=$(sysctl -a | grep brand_string | awk -F ':' '{print $2}')
-DISK=$(diskutil list | grep physical | awk -F ' ' '{print $1}')
+DISK=$(diskutil list | grep internal | awk -F ' ' '{print $1}')
 CAPACITY=$(diskutil info $DISK | grep 'Disk Size' | awk -F ' ' '{print $3$4}')
 MAX=$(ioreg -l -w0 | grep '\"MaxCapacity\" =' | awk -F ' ' '{print $5}') 
 DESIGN=$(ioreg -l -w0 | grep '\"DesignCapacity\" =' | awk -F ' ' '{print $5}') 
 BATT=$((100*$MAX/$DESIGN))
-
 echo 'CPU: '$CPU
 echo 'RAM: '$RAM'GB'
 echo 'Physical Storage: '$CAPACITY
