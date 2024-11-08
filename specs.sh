45d6053d7ddba45f55be838b2204e99cfcc6d97f
RealArtsn <105236007+RealArtsn@users.noreply.github.com>
2024-11-08 11:40:11 -0500

    Display marketing model

--- specs.sh
+++ specs.sh
@@ -8,6 +8,12 @@ MAX=$(ioreg -l -w0 | grep '\"MaxCapacity\" =' | awk -F ' ' '{print $5}')
 DESIGN=$(ioreg -l -w0 | grep '\"DesignCapacity\" =' | awk -F ' ' '{print $5}')
 BATT=$((100*$MAX/$DESIGN))
 CYCLES=$(ioreg -l -w0 | grep '"CycleCount" =' | awk -F ' ' '{print $5}')
+# Display model info if available
+# https://apple.stackexchange.com/questions/98080/can-a-macs-model-year-be-determined-with-a-terminal-command
+if ! defaults read ~/Library/Preferences/com.apple.SystemProfiler.plist 'ModelNames' 2>/dev/null | cut -sd '"' -f 4 | uniq | grep 'Mac'
+then
+    echo 'Model info not available.'
+fi
 echo 'CPU: '$CPU
 echo 'RAM: '$RAM'GB'
 # Display GPU info if system_profiler available
